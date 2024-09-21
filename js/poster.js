e64c804b4a8b3d0ebd5153c8bcec497daa6fe6a2
Oleksandr <oleksandrtode@gmail.com>
2024-09-21 23:03:05 +0300

    Created a new popup with some information from the cards in the poster section. After clicking the button tickets popup opents

--- js/poster.js
+++ js/poster.js
@@ -9,3 +9,38 @@ btnMore.addEventListener('click', () => {
     btnMore.textContent = 'less';
   }
 });
+
+const orderTicketsBtn = document.querySelectorAll('#orderTicketsBtn');
+
+const ticketsPopupContainer = document.querySelector('#tickets-popup');
+const ticketsPopupOverlay = document.querySelector('#ticketsPopupOverlay');
+const ticketsCrossIcon = document.querySelector('#tickets-cross-icon');
+const ticketsPopupDateDay = document.querySelector('.tickets-popup_dateDay');
+const ticketsPopupDateMonth = document.querySelector('.tickets-popup_dateMonth');
+const ticketsPopupName = document.querySelector('.tickets-popup_name');
+
+const openTicketsPopup = (event) => {
+  const card = event.target.closest(".Card");
+
+  const dateDay = card.querySelector(".CardDate div:first-child").textContent;
+  const dateMonth = card.querySelector(".CardDate div:last-child").textContent;
+  const eventName = card.querySelector(".CardInfoText p:first-child").textContent;
+
+  ticketsPopupDateDay.textContent = dateDay;
+  ticketsPopupDateMonth.textContent = dateMonth;
+  ticketsPopupName.textContent = eventName;
+
+  ticketsPopupContainer.style.display = 'block';
+  ticketsPopupOverlay.style.display = 'block';
+};
+
+const closeTicketsPopup = () => {
+  ticketsPopupContainer.style.display = 'none';
+  ticketsPopupOverlay.style.display = 'none';
+};
+
+orderTicketsBtn.forEach(button => {
+  button.addEventListener('click', openTicketsPopup);
+})
+
+ticketsCrossIcon.addEventListener('click', closeTicketsPopup);
