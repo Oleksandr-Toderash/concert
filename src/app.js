d4673af711b35eefce0539ec7b75cd7e5e6d8f7c
Oleksandr Toderash <logictab.comp@gmail.com>
2024-10-18 11:41:43 +0300

    Installed css loader

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-console.log('Hi Webpack!');
+import "./styles/css/footer.css";
+import "./styles/css/general.css";
+import "./styles/css/header.css";
+import "./styles/css/infoCards.css";
+import "./styles/css/newsCalendar.css";
+import "./styles/css/shortsAnonses.css";
+import "./styles/css/viewOpera.css";
